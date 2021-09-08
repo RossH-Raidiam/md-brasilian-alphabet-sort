4c5355038fe3c3f2bff5e9e2fcd7ede6095b6762
Ross Haliburton <ross.haliburton@raidiam.com>
2021-09-08 20:17:58 +0100

    initial commit with todos

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rh2g17/md-brasilian-alphabet-sort@main
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,5 @@
+package main
+
+//TODO: implement a hashmap for all Brazilian characters
+
+//TODO: implement an alphabet sort using the hashmap
